2c621adb9bc55204f26f03a1168e213f6a623753
cahaseler <cahaseler@gmail.com>
2024-05-26 19:26:03 -0400

    fix: ci config, added v1 logo and icons, updated scripts

--- web/app/(frontend)/page.tsx
+++ web/app/(frontend)/page.tsx
@@ -1,7 +1,15 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>EMPACT</div>
+      <Image
+        src="/empact-logo-transparent-cropped-square.png"
+        alt="Logo"
+        width={300}
+        height={300}
+      />
+      <div>Work in progress - more to come!</div>
     </main>
   );
 }
